07942be3a633f5efbb247fbe7c99776477837656
Tejas Nopany <61219536+tejasnopany@users.noreply.github.com>
2023-06-25 13:59:07 +0530

    Added style and Rectified code

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ getEmoji();
 document.getElementById("generate-btn").addEventListener("click", function() {
   let randomIndex = Math.floor(Math.random() * emojis.length);
   let randomEmoji = emojis[randomIndex];
-  document.getElementById("emoji").textContent = randomEmoji;
+  document.getElementById("emojis").textContent = randomEmoji;
 });
